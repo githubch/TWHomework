51583d3a480d09af24232af8a4ab70b5c5291a6f
githubch <944214536@qq.com>
2013-01-28 13:54:50 +0800

    20130128

--- SalaryManager/SalaryCaculator.java
+++ SalaryManager/SalaryCaculator.java
@@ -4,12 +4,4 @@ public class SalaryCaculator {
     int getBasicWage(int days){
         return days * 200;
     }
-
-    int getAward(int days){
-        return (int)(getBasicWage(days) * 0.1);
-    }
-
-    public int getAwardTax(int days){
-        return (int)(getAward(days) * 0.05);
-    }
 }
